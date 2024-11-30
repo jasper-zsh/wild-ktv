fd7c2decba5da7678fe8026703dcb20555b73878
Jasper.Z <jasper.zsh@hotmail.com>
2024-11-30 23:35:43 +0800

    add: improved search action

--- src/wild_ktv/uix/topbar.py
+++ src/wild_ktv/uix/topbar.py
@@ -12,10 +12,21 @@ Builder.load_file(os.path.join(os.path.dirname(__file__), 'topbar.kv'))
 logger = logging.getLogger(__name__)
 
 class TopBar(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.keyword = ''
+
+    def on_focus(self, focus: bool):
+        if focus:
+            app = App.get_running_app()
+            if app.screen_manager.current != 'search':
+                app.nav_push('search')
+
     def on_search(self, keyword: str):
-        logger.info(f'on_search {keyword}')
         app = App.get_running_app()
-        if app.screen_manager.current is not 'search':
+        if app.screen_manager.current != 'search':
             app.nav_push('search')
+        if self.keyword == keyword:
+            return
         screen: SearchScreen = app.screen_manager.get_screen('search')
         screen.search(keyword)
